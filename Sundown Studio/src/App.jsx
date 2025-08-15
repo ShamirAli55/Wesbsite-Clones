9cfb6b9bfddc9a09d5dcb64a126ba3cd78790f33
ShamirAli55 <shamirali9779@gmail.com>
2025-08-15 05:32:08 -0700

    Add Footer component and update App layout; enhance CSS with gradient background

--- Sundown Studio/src/App.jsx
+++ Sundown Studio/src/App.jsx
@@ -4,15 +4,20 @@ import VideoSection from './pages/VideoSection'
 import AboutSection from './pages/AboutSection'
 import Projects from './pages/Projects'
 import Slider from './pages/Slider'
+import Footer from './pages/Footer'
+
 const App = () => {
   return (
-    <div className='bg-[#EFEAE3] overflow-x-hidden'>
+    <>
+    <div className='bg-[#EFEAE3] overflow-x-hidden relative z-[10]'>
       <HeroSection />
       <VideoSection/>
       <AboutSection/>
       <Projects/>
       <Slider/>
     </div>
+    <Footer/>
+    </>
   )
 }
 
--- Sundown Studio/src/pages/Footer.jsx
+++ Sundown Studio/src/pages/Footer.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='h-screen relative bg-transparent text-[#EFEAE3]'>
+      <div className='fixed h-screen w-full gradient text-[#EFEAE3] z-[9] bottom-0 p-4'>
+
+      <div className='flex items-center justify-between px-18 pt-26 w-full'>
+          <div className='text-2xl font-bold'>
+          <h3>Work</h3>
+          <h3>Studio</h3>
+          <h3>Contact</h3>
+          </div>
+          <div className='w-92 pr-30'>
+            <h5>Get industry insights and creative inspiration straight into your inbox.</h5>
+            <p className='text-[#8e8989ee] border-b border-[#EFEAE3] pt-4'>Email Address</p>
+          </div>
+      </div>
+      
+      <div className='text-center font-bold px-4'>
+        <h1 className='text-[22vw] border-b border-[#efeae37b] leading-80'>Sundown</h1>
+        <div className='text-[#EFEAE3] flex items-center justify-between px-6 py-4 text-sm font-normal'>
+          <h5>Copyright &copy; Sundown Studio</h5>
+          <h5>Brooklyn,NY</h5>
+          <h5>Instagram</h5>
+          <h5>LinkedIn</h5>
+        </div>
+      </div>
+
+      </div>
+    </div>
   )
 }
 
